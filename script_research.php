bc410a0703763fa6e0300281ad62dc751d5ee9a0
mikouli <michaelvast@hotmail.fr>
2014-01-23 10:45:35 +0100

    HTML CSS
    Boostrap + fichiers liés

---
+++ script_research.php
@@ -0,0 +1,7 @@
+<?php
+
+$connexion = mysql_connect('sqletud.univ-mlv.fr',mvast,Zaosaa5x) or die('erreur de connexion');
+	mysql_select_db('mvast_db',$connexion) or die('erreur de base de donnée');
+
+echo("good");
+?>
